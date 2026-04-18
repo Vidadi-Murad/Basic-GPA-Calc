a49640a5c204023cada697706e60bb7053861b79
VidadiMurad <vidad0murad@gmail.com>
2026-04-18 12:36:01 +0400

    Add Variables for program

--- App.java
+++ App.java
@@ -3,4 +3,23 @@
 import java.util.Scanner;
 import java.util.Locale;
 
+public class App{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US); 
 
+        // Variables
+        int  phiyscs;
+        int math;
+        int english;
+        int history;
+        int music;
+        int chemistry;
+
+
+        
+
+
+        scanner.close();
+    }
+}
